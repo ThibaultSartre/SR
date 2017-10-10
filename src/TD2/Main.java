b78ab9053f70d340284af3a1168a1547b7af7604
Thibault <sartre.thibault@hotmail.fr>
2017-10-10 10:02:41 +0200

    TD2

--- src/TD2/Main.java
+++ src/TD2/Main.java
@@ -1,11 +1,13 @@
 package TD2;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by thsartre on 03/10/2017.
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Compte compte = new Compte("Sartre","Thibault",0);
         /*System.out.println(compte.toString());
         Thread1 th1 = new Thread1(compte);
         Thread2 th2 = new Thread2(compte);
@@ -16,6 +18,16 @@ public class Main {
         th2.join();
 
         System.out.println(compte.toString());*/
+        Scanner scanner = new Scanner(System.in);
+        String nom;
+        String prenom;
+        System.out.println("Entrez un nom");
+        nom = scanner.nextLine();
+        System.out.println("Entrez un prenom");
+        prenom = scanner.nextLine();
+
+        Compte compte = new Compte(nom,prenom,0);
+
         Thread3 th3 = new Thread3(compte);
         Thread3 th4 = new Thread3(compte);
 
@@ -28,5 +40,6 @@ public class Main {
         System.out.println(compte.toString());
         System.out.println("Retrait : " + compte.getTrace().getRetrait());
         System.out.println("Versement : " + compte.getTrace().getVersement());
+        System.out.println("Tab " + Arrays.toString(compte.getTrace().getFile()));
     }
 }
--- src/TD2/Trace.java
+++ src/TD2/Trace.java
@@ -8,7 +8,15 @@ public class Trace {
 
     private int versement;
 
+    public String[] getFile() {
+        return file;
+    }
+
     private int retrait;
+
+    private String[] file;
+
+    private int cpt;
 
     public int getVersement() {
         return versement;
@@ -20,14 +28,20 @@ public class Trace {
 
     public void IncVersement() {
         this.versement++;
+        this.file[this.cpt%5] = "V";
+        this.cpt++;
     }
 
     public Trace(){
         this.versement = 0;
         this.retrait = 0;
+        this.cpt = 0;
+        this.file = new String[5];
     }
 
     public void IncRetrait() {
         this.retrait++;
+        this.file[this.cpt%5] = "R";
+        this.cpt++;
     }
 }
